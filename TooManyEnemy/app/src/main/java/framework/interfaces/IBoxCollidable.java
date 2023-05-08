7cb567731eb2cc251f505c84daffd51211125261
lhy <ukrk774@kpu.ac.kr>
2023-05-08 21:25:40 +0900

    cloning framework
    수업 시간에 있던 프레임워크를 가져옵니다.

---
+++ TooManyEnemy/app/src/main/java/framework/interfaces/IBoxCollidable.java
@@ -0,0 +1,7 @@
+package framework.interfaces;
+
+import android.graphics.RectF;
+
+public interface IBoxCollidable {
+    public RectF getCollisionRect();
+}
---
+++ TooManyEnemy/app/src/main/java/framework/interfaces/IRecyclable.java
@@ -0,0 +1,5 @@
+package framework.interfaces;
+
+public interface IRecyclable {
+    public void onRecycle();
+}
