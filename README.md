72c490f6ab01222f971114cde61bd0dcf2b5e9e5
lhy <ukrk774@kpu.ac.kr>
2023-05-08 20:40:47 +0900

    add title highscore and debug button

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# SmartphoneGamingProgramming
-2023 스마트폰게임 프로그래밍의 원활한 수업 참여를 위한 Git Remote Repository입니다.
 # Too Many Enemy: 핵앤슬래시 로그라이크 게임
-
+2023 스마트폰게임 프로그래밍의 원활한 수업 참여를 위한 Git Remote Repository입니다.
+# 게임 설명
 ## 게임 컨셉
 ![1](https://user-images.githubusercontent.com/76027901/229520345-ec551c78-01a9-4ae0-b91f-e4fc17b4b0d1.png)
 ## 주요 개발 요소
@@ -11,3 +10,12 @@
 ![3](https://user-images.githubusercontent.com/76027901/229520658-655da7e6-08c2-46c7-9ca9-0458250f47d0.png)
 ## 개발 일정
 ![4](https://user-images.githubusercontent.com/76027901/229520914-1b62df8b-6b25-488b-afd0-6a8979523e62.png)
+# 코드 설명
+## 뷰 리스트
+### Title Activity
+- highScoreText : 하이스코어를 출력합니다.
+    - saveHighScore(int score) : SharedPreferences를 통해 하이스코어 저장
+    - getHighScore() : SharedPreferences를 통해 하이스코어 불러오기
+    - "MyPrefs"의 "high_score" 변수로 저장됩니다.
+- startGameButton : 게임 화면인 Main Activity를 시작합니다.
+- hsPlusOneBuuton : 디버그(테스트)용 버튼으로, High Score를 1 증가시켜 저장합니다.
--- TooManyEnemy/app/src/main/java/com/example/myapplication/TitleActivity.java
+++ TooManyEnemy/app/src/main/java/com/example/myapplication/TitleActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,14 +21,42 @@ public class TitleActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        setHighScore();
+
         // Start Game 버튼 클릭 이벤트 처리
         binding.startGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 클릭 이벤트 처리 코드 작성
                 Intent intent = new Intent(TitleActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
+
+        // High Score 버튼 클릭 이벤트 처리
+        binding.hsPlusOneBuuton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveHighScore(getHighScore()+1);
+                setHighScore();
+            }
+        });
+
     }
+    public void saveHighScore(int score) {
+        SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("high_score", score);
+        editor.apply();
+    }
+    public int getHighScore() {
+        SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        return prefs.getInt("high_score", 0);
+    }
+    private void setHighScore(){
+        int highScore = getHighScore();
+        binding.highScoreText.setText("High Score: " + highScore);
+    }
+
+
+
 }
